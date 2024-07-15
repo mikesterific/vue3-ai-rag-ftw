9de98793796d20846f31520a55b2c8719d69cfa1
mjones <mjones@scalecomputing.com>
2024-07-14 20:30:47 -0500

    tsconfig working

--- client/src/types/shims-vue.d.ts
+++ client/src/types/shims-vue.d.ts
@@ -1,6 +1,6 @@
 // src/types/shims-vue.d.ts
 declare module '*.vue' {
-  import { DefineComponent } from 'vue';
+  import type { DefineComponent } from 'vue';
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
